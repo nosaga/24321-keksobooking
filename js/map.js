c24c5a432a17bbb7e1a5392b0321725402a177ce
violetta <violetta.ashirova@gmail.com>
2018-09-29 12:30:12 +0300

    edtis to room check

--- js/map.js
+++ js/map.js
@@ -223,12 +223,14 @@ var guests = form.elements.capacity;
 
 guests.addEventListener('change', function (evt) {
   var target = evt.target;
-  if (parseInt(rooms.value, 10) === 100 && parseInt(target.value, 10) !== 0) {
+  var roomValue = parseInt(rooms.value, 10);
+  var guestValue = parseInt(target.value, 10);
+  if (roomValue === 100 && guestValue !== 0) {
     target.setCustomValidity('100 комнат не для гостей');
-  } else if (parseInt(target.value, 10) === 0 && parseInt(rooms.value, 10) !== 100) {
+  } else if (guestValue === 0 && roomValue !== 100) {
     target.setCustomValidity('100 комнат не для гостей');
-  } else if ((parseInt(rooms.value, 10)) >= (parseInt(target.value, 10))) {
-    target. setCustomValidity('');
+  } else if (roomValue >= guestValue) {
+    target.setCustomValidity('');
   } else {
     target.setCustomValidity('Количество гостей не может быть больше ' + rooms.value);
   }
