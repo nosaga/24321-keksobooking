5033bd9c4354b4ef527d12bef766f146ebc0a2b5
violetta <violetta.ashirova@gmail.com>
2018-11-07 22:19:35 +0300

    functions

--- js/form.js
+++ js/form.js
@@ -7,21 +7,16 @@
   var price = data.form.elements.price;
   var type = data.form.elements.type;
   var time = document.querySelector('.ad-form__element--time');
-
-
   var PRICING = {
     flat: 1000,
     house: 5000,
     palace: 10000,
     bungalo: 0
   };
-
   for (var l = 0; l < data.formFields.length; l++) {
     data.formFields[l].setAttribute('disabled', 'disabled');
   }
-  rooms.addEventListener('change', checkCapacity);
-  guests.addEventListener('change', checkCapacity);
-  function checkCapacity() {
+  var checkCapacity = function () {
     var roomValue = parseInt(rooms.value, 10);
     var guestValue = parseInt(guests.value, 10);
     if (roomValue === 100 && guestValue !== 0) {
@@ -33,17 +28,8 @@
     } else {
       guests.setCustomValidity('Количество гостей не может быть больше ' + rooms.value);
     }
-  }
-
-  type.addEventListener('change', function (evt) {
-    var target = evt.target;
-    var typeValue = target.value;
-    price.placeholder = PRICING[typeValue];
-    checkTypeAndPrice();
-  });
-
-  price.addEventListener('change', checkTypeAndPrice);
-  function checkTypeAndPrice() {
+  };
+  var checkTypeAndPrice = function() {
     var typeValue = type.value;
     var priceValue = price.value;
     if (parseInt(priceValue, 10) < PRICING[typeValue]) {
@@ -51,8 +37,17 @@
     } else {
       price.setCustomValidity('');
     }
-  }
+  };
 
+  rooms.addEventListener('change', checkCapacity);
+  guests.addEventListener('change', checkCapacity);
+  type.addEventListener('change', function (evt) {
+    var target = evt.target;
+    var typeValue = target.value;
+    price.placeholder = PRICING[typeValue];
+    checkTypeAndPrice();
+  });
+  price.addEventListener('change', checkTypeAndPrice);
   time.addEventListener('change', function (e) {
     var checkin = data.form.elements.timein;
     var checkout = data.form.elements.timeout;
