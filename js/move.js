9bf6ec697b8717cec648093b995a833865214453
violetta <violetta.ashirova@gmail.com>
2018-11-07 23:23:28 +0300

    function fixes

--- js/move.js
+++ js/move.js
@@ -7,45 +7,14 @@
 
   data.mapPinMain.addEventListener('mousedown', function (e) {
     e.preventDefault();
-    var coords = getCoords(data.mapPinMain);
-    var shiftX = e.pageX - coords.pageX;
-    var shiftY = e.pageY - coords.pageY;
-    var adsCoords = data.adsDialog.getBoundingClientRect();
-
-    document.body.insertAdjacentElement('afterbegin', data.mapPinMain);
-
-    moveAt(coords);
-    function onMouseMove(moveEvt) {
-      moveEvt.preventDefault();
-      var mapCoords = setBounds(moveEvt);
-      moveAt(mapCoords);
-      return setFormCoords(mapCoords.pageX + data.MAIN_PIN_WIDTH / 2, mapCoords.pageY + data.MAIN_PIN_HEIGHT);
-    }
-
-    function onMouseUp(upEvt) {
-      upEvt.preventDefault();
-      document.removeEventListener('mousemove', onMouseMove);
-      document.removeEventListener('mouseup', onMouseUp);
-    }
-
-    function getCoords(elem) {
+    var getCoords = function (elem) {
       var box = elem.getBoundingClientRect();
       return {
         pageX: box.left + window.pageXOffset,
         pageY: box.top + window.pageYOffset
       };
-    }
-
-    function moveAt(evt) {
-      data.mapPinMain.style.left = evt.pageX + 'px';
-      data.mapPinMain.style.top = evt.pageY + 'px';
-    }
-
-    function setFormCoords(x, y) {
-      data.formAddress.value = Math.floor(x) + ', ' + Math.floor(y);
-    }
-
-    function setBounds(evt) {
+    };
+    var setBounds = function (evt) {
       var coordsOnMove = {
         pageY: evt.pageY - shiftY,
         pageX: evt.pageX - shiftX,
@@ -65,8 +34,33 @@
       }
 
       return coordsOnMove;
-    }
+    };
+    var setFormCoords = function (x, y) {
+      data.formAddress.value = Math.floor(x) + ', ' + Math.floor(y);
+    };
+    var onMouseMove = function (moveEvt) {
+      moveEvt.preventDefault();
+      var mapCoords = setBounds(moveEvt);
+      moveAt(mapCoords);
+      return setFormCoords(mapCoords.pageX + data.MAIN_PIN_WIDTH / 2, mapCoords.pageY + data.MAIN_PIN_HEIGHT);
+    };
 
+    var onMouseUp = function (upEvt) {
+      upEvt.preventDefault();
+      document.removeEventListener('mousemove', onMouseMove);
+      document.removeEventListener('mouseup', onMouseUp);
+    };
+    var coords = getCoords(data.mapPinMain);
+    var shiftX = e.pageX - coords.pageX;
+    var shiftY = e.pageY - coords.pageY;
+    var adsCoords = data.adsDialog.getBoundingClientRect();
+
+    document.body.insertAdjacentElement('afterbegin', data.mapPinMain);
+    moveAt(coords);
+    function moveAt(evt) {
+      data.mapPinMain.style.left = evt.pageX + 'px';
+      data.mapPinMain.style.top = evt.pageY + 'px';
+    }
 
     document.addEventListener('mousemove', onMouseMove);
     document.addEventListener('mouseup', onMouseUp);
